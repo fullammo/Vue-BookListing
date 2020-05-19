68f7eec04265d0feac47f545db5c6a938611062f
bendeguz.gulyas <bendeguz.gulyas@risingstack.com>
2020-05-19 18:05:21 +0200

    bookItmem was untracked

---
+++ src/components/BookItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <li>{{ book.title }}:{{ book.author }}</li>
+</template>
+
+<script>
+export default {
+  name: "BookItem",
+  props: ["book"],
+};
+</script>
+
+<style>
+li {
+  margin: 0 10px;
+  display: block;
+}
+</style>
